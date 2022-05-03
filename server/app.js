54a683b466113b7745303ea653a63af2c8627cc0
Harshit Behl <harshit.behl.96@gmail.com>
2022-05-02 22:27:05 -0400

    feat(server): add mongodb setup with mongoose & implement routes

--- server/app.js
+++ server/app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
+// Router Imports
+const sharkRouter = require("./routes/sharkRoutes");
+const catRouter = require("./routes/catRoutes");
+
 // Enable Cors
 app.use(cors({ origin: "*" }));
 
@@ -9,12 +13,8 @@ app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 // Routes
-app.get("/api/v1/test", (req, res) => {
-  res.status(200).json({
-    status: "success",
-    message: "Test Successful!",
-  });
-});
+app.use("/api/v1/sharks", sharkRouter);
+app.use("/api/v1/cats", catRouter);
 
 // App Export
 module.exports = app;
