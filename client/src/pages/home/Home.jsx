becb314f319052d22a957111deba196b145df674
Harshit Behl <harshit.behl.96@gmail.com>
2022-05-03 05:46:08 -0400

    feat(showcase): add showcase commponent & showcase animations

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "./Home.scss";
 
+import Showcase from "../../components/showcase/Showcase";
+// import Task from "../../components/task/Task";
+
 function Home() {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      {/* Showcase Section */}
+      <Showcase />
+      {/* Task Section */}
+      {/* <Task /> */}
+    </div>
+  );
 }
 
 export default Home;
